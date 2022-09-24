761d96f7d6771e21bba7c68200be84326053e069
costacarlos100 <chcostaalves1974@gmail.com>
2022-09-24 18:07:47 -0300

    sistema de tabuada

--- src/main.java
+++ src/main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+			int numero = in.nextInt();
+			
+		for(int i = 0; i<=10; i++) {
+			System.out.println(i+" * "+numero+" = "+i*numero);
+		}
 
 	}
 
